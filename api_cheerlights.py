81401a698273046b5ab384f0cd8b8f40474bff88
russ <russell.winch@gmail.com>
2017-12-04 19:44:00 +0000

    replaced global variables with discrete functions

--- api_cheerlights.py
+++ api_cheerlights.py
@@ -6,7 +6,7 @@ A counter is reset with each new color and incremented with each
 subsequent same color cycle.
 
 @author George Kaimakis and Russ Winch
-@version 3 Dec 2017
+@version December 2017
 '''
 
 import urequests
@@ -16,94 +16,95 @@ import neopixel
 from machine import Pin
 from wifi import Wifi
 
-# Global variables:
-RECVD_COLOR         = ''
-PREVIOUS_COLOR      = ''
-NEW_COLOR_VAL       = ''
-OLD_COLOR_VAL       = ''
-
-INTERVAL            = 10000
-
-host                = 'https://thingspeak.com/'
-topic               = 'channels/1417/feeds/last.json'
-api                 = host + topic
-
-
-# look-up color dict - api 'field1' is used as the key:
-colors = {
-    'red':(255,0,0),
-    'orange':(255,30,0),
-    'yellow':(255,110,1),
-    'green':(0,255,0),
-    'cyan':(0,255,255),
-    'blue':(0,0,255),
-    'purple':(128,0,128),
-    'magenta':(255,0,50),
-    'pink':(255,40,50),
-    'white':(255,255,170),
-    'oldlace':(255,150,50),
-    'warmwhite':(255,150,50)
-    }
-
 def api_request(url):
-    global RECVD_COLOR
     feed = urequests.get(url)
-    color = feed.json()['field1']
-    RECVD_COLOR = color
+    return feed.json()['field1']
 
-def recvd_color_test(color):
-    global NEW_COLOR_VAL
+def recvd_color_test(color, colors):
     if color in colors:
-        NEW_COLOR_VAL = colors[color]
+        return colors[color]
+    print("not a valid Cheerlights colour")
+    return colors['red'] # default to red
 
-def new_neopixel_color(next_color):
+def new_neopixel_color(neo, next_color):
     for i in range(len(neo)):
         neo[i].fill(next_color)
         neo[i].write()
         time.sleep_ms(urandom.getrandbits(11))
 
-def neopixel_blank():
+def neopixel_blank(neo):
     for i in range(len(neo)):
         neo[i].fill((0,0,0))
         neo[i].write()
 
-def color_transition():
-    global NEW_COLOR_VAL
-    global OLD_COLOR_VAL
+# def color_transition():
+#     global NEW_COLOR_VAL
+#     global OLD_COLOR_VAL
 
-neo         = [] # holder for the neo pixels
-pixel_pins  = [0,14,12,13,15] # D3,D5,D6,D7,D8
-num_pixels  = 1 # leds per strip
+def main():
+    # look-up color dict - api 'field1' is used as the key:
+    colors = {
+        'red':(255,0,0),
+        'orange':(255,30,0),
+        'yellow':(255,110,1),
+        'green':(0,255,0),
+        'cyan':(0,255,255),
+        'blue':(0,0,255),
+        'purple':(128,0,128),
+        'magenta':(255,0,50),
+        'pink':(255,40,50),
+        'white':(255,255,170),
+        'oldlace':(255,150,50),
+        'warmwhite':(255,150,50)
+        }
 
-# define pins and create neopixel objects:
-for i in range(len(pixel_pins)):
-    pin = Pin(pixel_pins[i], Pin.OUT)
-    neo.append(neopixel.NeoPixel(pin, num_pixels))
+    recvd_color     = ''
+    prev_color      = ''
+    # NEW_COLOR_VAL       = ''
+    # OLD_COLOR_VAL       = ''
 
-# turn off any lit neopixels:
-neopixel_blank()
+    interval            = 15 # seconds delay between updates
 
-wifi = Wifi()
-wifi.connect()
+    host                = 'https://thingspeak.com/'
+    topic               = 'channels/1417/feeds/last.json'
+    api                 = host + topic
 
-count = 0
-while True:
-    api_request(api)
+    neopixels   = [] # holder for the neo pixels
+    pixel_pins  = [0,14,12,13,15] # D3,D5,D6,D7,D8
+    num_pixels  = 1 # leds per strip
 
-    # Check if color has changed:
-    # -if color is unchanged, increment counter, print and continue
-    if RECVD_COLOR == PREVIOUS_COLOR:
-        count += 1
-        print(str(count) + ': ' + RECVD_COLOR)
+    # define pins and create neopixel objects:
+    for i in range(len(pixel_pins)):
+        pin = Pin(pixel_pins[i], Pin.OUT)
+        neopixels.append(neopixel.NeoPixel(pin, num_pixels))
 
-    # -if color has changed, reset counter, print, extract colot value form dict
-    # -and regenerate neopixel with new color.
-    # -reset color varaiables
-    else:
-        count = 1
-        print(str(count) + ': ' + RECVD_COLOR)
-        recvd_color_test(RECVD_COLOR)
-        new_neopixel_color(NEW_COLOR_VAL)
-        PREVIOUS_COLOR = RECVD_COLOR
-        OLD_COLOR_VAL = NEW_COLOR_VAL
-    time.sleep_ms(INTERVAL)
+    # turn off any lit neopixels:
+    neopixel_blank(neopixels)
+
+    # connect wifi
+    wifi = Wifi()
+    wifi.connect()
+
+    count = 0
+
+    while True:
+        recvd_color = api_request(api)
+
+        # Check if color has changed:
+        if recvd_color == prev_color:
+            count += 1
+            print(str(count) + ': ' + recvd_color)
+
+        # if color has changed, reset counter, print, extract color from dict
+        else:
+            count = 1
+            print(str(count) + ': ' + recvd_color)
+            prev_color = recvd_color
+            recvd_color = recvd_color_test(recvd_color, colors)
+            new_neopixel_color(neopixels, recvd_color)
+
+        # sleep till the next update
+        time.sleep(interval)
+
+# run the main function
+main()
